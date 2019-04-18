5c82c90adbd5808832ebd3c0bee9ffdad14b3117
WillemTaylor <willemtaylor@msn.com>
2019-04-18 15:25:07 +0100

    changes made for deployment1

---
+++ build/precache-manifest.e27b91dc08408ae0041fb8b1c2a31eb9.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "acb27dd9b9a7cf9cc412",
+    "url": "/static/css/main.732f5029.chunk.css"
+  },
+  {
+    "revision": "acb27dd9b9a7cf9cc412",
+    "url": "/static/js/main.e3eae7ec.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "9343167ab92c1826a4a4",
+    "url": "/static/js/2.9dd2b59e.chunk.js"
+  },
+  {
+    "revision": "f7a8463eeb8134ebdcbba438ff42d69e",
+    "url": "/static/media/yt.f7a8463e.png"
+  },
+  {
+    "revision": "599c5e041744aa6ea97c297ea9aeabbc",
+    "url": "/static/media/logo.599c5e04.png"
+  },
+  {
+    "revision": "83a50cab81cb54207adb702c1118b853",
+    "url": "/static/media/insta1.83a50cab.jpeg"
+  },
+  {
+    "revision": "fe7826142530ef695139b44e400a5081",
+    "url": "/static/media/insta5.fe782614.jpeg"
+  },
+  {
+    "revision": "9564bbc1bd584ead9ff2d06cdb5ca468",
+    "url": "/static/media/insta3.9564bbc1.jpeg"
+  },
+  {
+    "revision": "a6fb3e0350b5ec0b863b9e81c0d5f188",
+    "url": "/static/media/insta6.a6fb3e03.jpeg"
+  },
+  {
+    "revision": "894702ab19ce9426c0c174c0772dd820",
+    "url": "/static/media/insta2.894702ab.jpeg"
+  },
+  {
+    "revision": "c0ed9e8710680ee616c8afe679a1ba5f",
+    "url": "/static/media/insta7.c0ed9e87.jpeg"
+  },
+  {
+    "revision": "0dec692dbe4fa16b7dd0754730a0d17f",
+    "url": "/static/media/insta4.0dec692d.jpeg"
+  },
+  {
+    "revision": "a1f0e741d86ba58c5449c9cda979661f",
+    "url": "/static/media/insta8.a1f0e741.jpeg"
+  },
+  {
+    "revision": "843228f9de02124e21890d170164926e",
+    "url": "/index.html"
+  }
+];
